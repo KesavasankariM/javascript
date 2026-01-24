e16ffc5a26428db9efb77c8e8c4ecc00e97d5813
Kesavasankari <146357096+KesavasankariM@users.noreply.github.com>
2026-01-24 11:32:45 +0530

    Add files via upload

---
+++ 3_hoisiting/hoisting.js
@@ -0,0 +1,15 @@
+//hoisting
+// console.log("hi")
+// console.log("hello")
+// console.log("bye")
+// console.log(a)
+// let a=10
+// console.log(a)
+// let b=20
+
+//! functional hoisting
+abc()
+function abc(){
+    console.log("i am from function")
+}
+
